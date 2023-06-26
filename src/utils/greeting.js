2c647693b8d2144a1e2385947178999bb63fba7c
Yu Mentsel <yu.mentsel@gmail.com>
2023-06-26 23:39:58 +0300

    feat: add basic operations with files

---
+++ src/utils/greeting.js
@@ -0,0 +1,14 @@
+import { greetingMessage, exitMessage } from './messages.js';
+
+export default () => {
+  const DEFAULT_USERNAME = 'Anonymous';
+
+  const args = process.argv.slice(2);
+  let usernameArg = args.find((el) => el.startsWith('--username'));
+  const username = usernameArg ? usernameArg.replace('--username=', '').trim() : DEFAULT_USERNAME;
+
+  // process.on('SIGINT', () => app.exit());
+  process.on('exit', () => exitMessage(username));
+
+  greetingMessage(username);
+};
